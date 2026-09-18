62df2ebd69aa9a75415b38411dcb0ce5d565aa72
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-18 09:25:28 +0700

    helo

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    string sentence;
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);
+
+    cout << "Length of the sentence: " << sentence.length() << endl;
+
+    size_t pos = sentence.find("C++");
+
+    if (pos != string::npos) {
+        cout << "Found \"C++\" at position: " << pos << endl;
+        cout << "Extracted word: " << sentence.substr(pos, 3) << endl;
+
+        sentence.replace(pos, 3, "Programming");
+        cout << "New sentence: " << sentence << endl;
+    } else {
+        cout << "The word \"C++\" was not found in the sentence." << endl;
+    }
+
     return 0;
 }
