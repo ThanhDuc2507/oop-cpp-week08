131eba6dc7edec956fc097b0a8f9fd80f4bf838f
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-22 23:31:24 +0700

    das

--- main.cpp
+++ main.cpp
@@ -2,16 +2,13 @@
 #include <string>
 using namespace std;
 
-void swapStr(string& a, string& b) {
-    string temp = a;
-    a = b;
-    b = temp;
+void changeFirstChar(string& str) {
+    str[0] = 'J';
 }
 
 int main() {
-    string x = "cat";
-    string y = "dog";
-    swapStr(x, y);
-    cout << x << " " << y << endl;
+    string name = "python";
+    changeFirstChar(name);
+    cout << name << endl;
     return 0;
 }
