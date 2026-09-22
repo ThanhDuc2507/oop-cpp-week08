09b72540889b06c42a202f21740be8b9ddbf6c6a
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-22 23:08:49 +0700

    das

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 
-void swapValues(int& x, int& y) {
-    int temp = x;
-    x = y;
-    y = temp;
+void changeValue(int x) {
+    x = 100;
+}
+
+void changeRef(int& x) {
+    x = 200;
 }
 
 int main() {
-    int a = 4, b = 9;
-    swapValues(a, b);
-    cout << a << " " << b << endl;
+    int a = 5;
+    changeValue(a);
+    cout << "After changeValue: " << a << endl;
+    changeRef(a);
+    cout << "After changeRef: " << a << endl;
     return 0;
 }
