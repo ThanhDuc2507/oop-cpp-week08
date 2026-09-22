3faa7d56f2ec51c2fb036da1426d012b8c14bcd9
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-22 22:12:12 +0700

    dad

--- main.cpp
+++ main.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-// Hàm hoán đổi hai số sử dụng tham chiếu
-void swapValues(int& x, int& y) {
-    int temp = x;
-    x = y;
-    y = temp;
-}
-
 int main() {
-    int a = 5, b = 10;
-    
-    cout << "Before: a = " << a << ", b = " << b << endl;
-    
-    // Gọi hàm hoán đổi
-    swapValues(a, b);
-    
-    cout << "After: a = " << a << ", b = " << b << endl;
-    
+    string s = "Hello C++";
+
+    cout << s.length() << endl;
+    cout << s.size() << endl;
+
     return 0;
 }
