1a4f4f052de0196719f06decd91640b50519a5cc
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-22 23:30:02 +0700

    das

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
-#include <vector>
+#include <string>
 using namespace std;
 
-void insertSubarray(vector<int>& arr, const vector<int>& sub, int index) {
-    arr.insert(arr.begin() + index, sub.begin(), sub.end());
-}
-
 int main() {
-    vector<int> nums = {10, 20, 30};
-    vector<int> sub = {7, 8, 9};
-    int pos = 1;
-
-    insertSubarray(nums, sub, pos);
-
-    for (int x : nums) {
-        cout << x << " ";
-    }
+    string s = "Hello";
+    string& r = s;
+    r += " World";
+    cout << s << endl;
+    cout << r << endl;
     return 0;
 }
