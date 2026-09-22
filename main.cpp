2ef14485d8b25aaa2de50ba3565934434f140608
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-22 22:14:38 +0700

    dad

--- main.cpp
+++ main.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 
 int main() {
-    string text = "I love C++";
-    size_t pos = text.find("C++");
+    string s = "Hello World";
 
-    if (pos != string::npos) {
-        cout << "Found at: " << pos << endl;
-    } else {
-        cout << "Not found" << endl;
-    }
+    string part1 = s.substr(0, 5);
+    string part2 = s.substr(6, 5);
+
+    cout << part1 << endl;
+    cout << part2 << endl;
 
     return 0;
 }
