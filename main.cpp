b2e03134c84d951e1938c52bd86ce03e50504699
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-22 22:43:31 +0700

    das

--- main.cpp
+++ main.cpp
@@ -3,19 +3,12 @@
 using namespace std;
 
 int main() {
-    string name, id, email, hometown;
+    string name;
 
     getline(cin, name);
-    getline(cin, id);
-    getline(cin, email);
-    getline(cin, hometown);
 
-    cout << "===== STUDENT PROFILE =====" << endl;
-    cout << "Full name   : " << name << endl;
-    cout << "Student ID  : " << id << endl;
-    cout << "Email       : " << email << endl;
-    cout << "Hometown    : " << hometown << endl;
-    cout << "===========================" << endl;
+    cout << "Hello, " << name << "!" << endl;
+    cout << "Welcome to C++ programming!" << endl;
 
     return 0;
 }
