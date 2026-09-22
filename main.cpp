381a2bd96b3e3c6bd1b0c58f550a52aae4129299
Huynh Nguyen Thanh Duc <25tm14503@student.hcmute.edu.vn>
2026-09-22 22:15:12 +0700

    dad

--- main.cpp
+++ main.cpp
@@ -3,13 +3,11 @@
 using namespace std;
 
 int main() {
-    string s = "Hello World";
+    string s = "Hello";
 
-    string part1 = s.substr(0, 5);
-    string part2 = s.substr(6, 5);
+    s.append(" C++");
 
-    cout << part1 << endl;
-    cout << part2 << endl;
+    cout << s << endl;
 
     return 0;
 }
